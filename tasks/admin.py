3741ed11e915a0d90d2144574347e1fc54829c63
rickxyo <pedrinhooldoni@gmail.com>
2026-04-05 21:46:25 -0300

    feat(tasks): customize Task model admin

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Task
 
-admin.site.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['user', 'title', 'status', 'created_at']
+    list_filter = ['status', 'created_at']
+    search_fields = ['title', 'user__username']
+
+admin.site.register(Task, TaskAdmin)
 
